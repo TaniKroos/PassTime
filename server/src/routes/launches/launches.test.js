040c6a7bc8fc1a70b0c502c2bc7ccb803da7ff86
TaniKroos <tanishsaini26@gmail.com>
2024-08-08 21:42:20 +0530

    :)

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,13 +1,19 @@
+const request = require('supertest');
+const app = require('../../app')
+
+
 describe('Test GET /launches', () =>{
-    test('It should response with 200 success' , () =>{
-        const response = 200;
-        expect(response).toBe(200);
+    test('It should response with 200 success' ,async () =>{
+        const response = await request(app)
+            .get('/launches')
+            .expect(200);
     });
 });
-
+ 
 describe('Test POST /launch ',() => {
-    test('It should respond with 200 success',() =>{
+    test('It should respond with 201 success',async () =>{
 
+       
     });
 });
 
