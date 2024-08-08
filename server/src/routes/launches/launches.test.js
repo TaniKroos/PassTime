2adeefe725fa2a183538529c4fcbb8b272f1ca7b
TaniKroos <tanishsaini26@gmail.com>
2024-08-08 20:22:37 +0530

    jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe('Test GET /launches', () =>{
+    test('It should response with 200 success' , () =>{
+        const response = 200;
+        expect(response).toBe(200);
+    });
+});
+
+describe('Test POST /launch ',() => {
+    test('It should respond with 200 success',() =>{
+
+    });
+});
+
+// these aren't built in Js functions 
